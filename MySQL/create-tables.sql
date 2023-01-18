acf0bd07724848b6160adbf865f1cbaede2be9fc
w00kster <26447183+w00kster@users.noreply.github.com>
2023-01-18 15:41:10 +1000

    Create create-tables.sql
    Guess at a basic structure for tables.

---
+++ MySQL/create-tables.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Leaderboard (
+    LeaderboardID INT AUTO_INCREMENT PRIMARY KEY,
+    MapID INT NOT NULL,
+    UserID INT NOT NULL,
+    OverallTime TIME NOT NULL,
+    Lap1Time TIME NOT NULL,
+    Lap2Time TIME NOT NULL,
+    Lap3Time TIME NOT NULL,
+    FOREIGN KEY (MapID) REFERENCES Maps(MapID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+);
+
+CREATE TABLE Cups (
+    CupID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Description TEXT,
+    Date DATE
+);
+
+CREATE TABLE Maps (
+    MapID INT AUTO_INCREMENT PRIMARY KEY,
+    CupID INT NOT NULL,
+    Name VARCHAR(255) NOT NULL,
+    Description TEXT,
+    FOREIGN KEY (CupID) REFERENCES Cups(CupID)
+);
+
+CREATE TABLE Users (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL
+    -- ,
+    -- Email VARCHAR(255) NOT NULL,
+    -- Password VARCHAR(255) NOT NULL
+);
