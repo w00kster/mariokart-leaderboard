38a3c4b7555991ef7a83185d74a73b5dd9d2a75b
w00kster <26447183+w00kster@users.noreply.github.com>
2023-03-02 01:47:09 +0000

    Re-define .gitignore for Terraform Add some docs for infrastructure Basic Terraform for MySQL cluster in Linode & IP allow list

---
+++ infrastructure/mysql/db.tf
@@ -0,0 +1,16 @@
+resource "linode_database_mysql" "mydatabase" {
+  label     = "mydatabase"
+  engine_id = "mysql/8.0.26"
+  region    = "ap-southeast"
+  type      = "g6-nanode-1"
+}
+
+resource "linode_database_mysql" "mariokart-leaderboard" {
+  label          = "mariokart-leaderboard"
+  engine_id      = "mysql/8.0.26"
+  region         = "ap-southeast"
+  type           = "g6-nanode-1"
+  allow_list     = ["220.245.113.214/32","193.116.209.48/32","180.150.36.8/32"]
+  ssl_connection = true
+}
+# terraform import linode_database_mysql.mariokart-leaderboard 15150
