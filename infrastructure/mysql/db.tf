e29180d343a0e5c436be74566a13a55d8d5a46ad
rosie.maguire <rosie.maguire@xplortechnologies.com>
2023-03-28 12:42:15 +1000

    Update IP Address that has changed

--- infrastructure/mysql/db.tf
+++ infrastructure/mysql/db.tf
@@ -10,7 +10,7 @@ resource "linode_database_mysql" "mariokart-leaderboard" {
   engine_id      = "mysql/8.0.26"
   region         = "ap-southeast"
   type           = "g6-nanode-1"
-  allow_list     = ["220.245.113.214/32","193.116.209.48/32","180.150.36.8/32"]
+  allow_list     = ["220.245.113.214/32","110.175.136.50/32","180.150.36.8/32"]
   ssl_connection = true
 }
 # terraform import linode_database_mysql.mariokart-leaderboard 15150
