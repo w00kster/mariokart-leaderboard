6dc1561f4a2356b489d1dabbc2640a445f9ff269
w00kster <26447183+w00kster@users.noreply.github.com>
2023-04-14 06:56:19 +0000

    Update engine version to current

--- infrastructure/mysql/db.tf
+++ infrastructure/mysql/db.tf
@@ -1,8 +1,6 @@
-
-
 resource "linode_database_mysql" "mariokart-leaderboard" {
   label          = "mariokart-leaderboard"
-  engine_id      = "mysql/8.0.26"
+  engine_id      = "mysql/8.0.30"
   region         = "ap-southeast"
   type           = "g6-nanode-1"
   allow_list     = ["220.245.113.214/32", "193.116.209.48/32", "180.150.36.8/32"]
