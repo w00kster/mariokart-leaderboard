a146187a641dfc3a2af15fe236c46ea9cca15415
rosiemaguire <rosemarymaguire19@yahoo.com.au>
2023-08-24 18:42:30 +1000

    Sorting out the carousel arrows

--- MarioKart Leaderboard UI/index.js
+++ MarioKart Leaderboard UI/index.js
@@ -11,8 +11,11 @@ function findTotal(){
 
 document.addEventListener("DOMContentLoaded", function(){
   $('.carousel').slick({
+    centerMode: true,
+    centerPadding: '60px',
     slidesToShow: 3,
-    slidesToScroll: 1,
-    arrows: true
+    arrows: true,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='images/icons8-previous-50.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='images/icons8-next-50.png'>"
   });
 });
