8ad433ee0d8bae43e82a757a603b5d55622484e7
rosie.maguire <rosie.maguire@xplortechnologies.com>
2023-10-03 14:21:31 +1000

    Got successful connection returned locally

--- MarioKart Leaderboard UI/connect.js
+++ MarioKart Leaderboard UI/connect.js
@@ -1,28 +1,55 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv').config();
 const isLocal = true;
-const connection = mysql.createConnection({
-  host: isLocal ? process.env.localhost : process.env.host,
-  user: isLocal ? process.env.localuser: process.env.user,
-  password: isLocal ? process.env.localpassword : process.env.password,
-  database: process.env.database,
-  ssl: {
-    ca: isLocal ? "" : process.env.ssl,
-    rejectUnauthorized: false
-  }
-});
 
-connection.connect(function(err) {
-  if (err) {
-    return console.error('error: ' + err.message);
-  }
+if (isLocal){
 
-  console.log('Connected to the MySQL server.');
-});
+  const localConnection =  mysql.createConnection({
+    host:process.env.localhost,
+    user: process.env.localuser,
+    password: process.env.localpassword ,
+    database: process.env.database,
+    port: process.env.localport
+  });
 
-connection.end(function(err) {
-  if (err) {
-    return console.log('error:' + err.message);
-  }
-  console.log('Close the database connection.');
-});
+  localConnection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
+  
+  localConnection.end(function(err) {
+    if (err) {
+      return console.log('error:' + err.message);
+    }
+    console.log('Close the database connection.');
+  });
+} else {
+  const connection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    password:process.env.password,
+    database: process.env.database,
+    ssl: {
+      ca: process.env.ssl,
+      rejectUnauthorized: false
+    }
+  });
+  connection.connect(function(err) {
+    if (err) {
+      return console.error('error: ' + err.message);
+    }
+  
+    console.log('Connected to the MySQL server.');
+  });
+  
+  connection.end(function(err) {
+    if (err) {
+      return console.log('error:' + err.message);
+    }
+    console.log('Close the database connection.');
+  });
+  
+}
